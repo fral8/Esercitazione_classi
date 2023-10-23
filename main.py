828fb34db03ca5759f47c42a5f1332940ece967a
FedericoCea <federicocea7@gmail.com>
2023-10-23 12:19:34 +0200

    Esercizio 3: Auto

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from AppEngine.Persona import Persona
-
+from AppEngine.Automobile import Automobile
 
 
 if __name__=="__main__":
@@ -9,3 +9,31 @@ if __name__=="__main__":
     persona1.setSesso("m")
     persona1.setAge(32)
     persona1.presentati()
+    
+    print("ESERCIZIO 3:\n")
+    automobile1=Automobile("","","")
+    automobile2=Automobile("","","")
+    automobile3=Automobile("","","")
+    automobile4=Automobile("","","")
+    automobile5=Automobile("","","")
+    automobile1.set_marca("Toyota")
+    automobile2.set_marca("Ford")
+    automobile3.set_marca("Honda")
+    automobile4.set_marca("BMW")
+    automobile5.set_marca("Volkswagen")
+    automobile1.set_modello("Corolla")
+    automobile2.set_modello("Mustang")
+    automobile3.set_modello("Civic")
+    automobile4.set_modello("X5")
+    automobile5.set_modello("Golf")
+    automobile1.set_anno("2019")
+    automobile2.set_anno("2022")
+    automobile3.set_anno("2020")
+    automobile4.set_anno("2021")
+    automobile5.set_anno("2018")
+    automobile1.descrizione()
+    automobile2.descrizione()
+    automobile3.descrizione()
+    automobile4.descrizione()
+    automobile5.descrizione()
+    
